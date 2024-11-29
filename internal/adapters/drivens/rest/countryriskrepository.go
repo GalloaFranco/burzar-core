36c4ad6d54bc5f23878aabc76a493c2de1e8f529
GalloaFranco <galloafranco@gmail.com>
2024-11-29 01:17:39 -0300

    CountryRisk repo

---
+++ internal/adapters/drivens/rest/countryriskrepository.go
@@ -0,0 +1,34 @@
+package driven_rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/GalloaFranco/burzar-core/internal/core/domain"
+)
+
+type CountryRiskRepository struct {
+	client  *http.Client
+	baseURL string
+}
+
+func NewCountryRiskRepository(client *http.Client, baseURL string) *CountryRiskRepository {
+	return &CountryRiskRepository{client: client, baseURL: baseURL}
+}
+
+func (c *CountryRiskRepository) Obtain() (*domain.CountryRisk, error) {
+	response, err := c.client.Get(c.baseURL)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: status %d", response.StatusCode)
+	}
+
+	var crr CountryRiskResponse
+	if err := json.NewDecoder(response.Body).Decode(&crr); err != nil {
+		return nil, err
+	}
+	return crr.ToCountryRisk(), nil
+}
